dfb8be7383d72ae45ddc13d8121c823e247a0e0c
joaomarcos11 <joaomarcos11.tech@gmail.com>
2020-07-29 09:08:15 -0300

    Implementacao de React Router e pages

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
-// import ButtonLink from '../ButtonLink';
 import Button from '../Button';
 
 function Menu() {
   return (
     <nav className="Menu">
-      <a href="/">
+      <Link to="/">
         <img className="Logo" src={Logo} alt="Logomarca Aluraflix" />
-      </a>
-      <Button as="a" href="/" className="ButtonLink">
+      </Link>
+      <Button as={Link} to="/cadastro/video" className="ButtonLink">
         Novo vídeo
       </Button>
       {/* <ButtonLink href="/" className="ButtonLink">
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Menu from './components/Menu';
-import BannerMain from './components/BannerMain';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer';
+import Menu from '../../components/Menu';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
 
-import dadosIniciais from './data/dados_iniciais.json';
+import dadosIniciais from '../../data/dados_iniciais.json';
 
-function App() {
+function Home() {
   return (
     <div style={{ backgroundColor: '#141414' }}>
       <Menu />
@@ -23,10 +23,12 @@ function App() {
       <Carousel category={dadosIniciais.categorias[1]} />
       <Carousel category={dadosIniciais.categorias[2]} />
       <Carousel category={dadosIniciais.categorias[3]} />
+      <Carousel category={dadosIniciais.categorias[4]} />
+      <Carousel category={dadosIniciais.categorias[5]} />
 
       <Footer />
     </div>
   );
 }
 
-export default App;
+export default Home;
